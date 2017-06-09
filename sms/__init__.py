def9409823fd1227ba9e98a60a73dc754c14dab1
Harsh Kapoor <hkapoordroid@gmail.com>
2017-06-08 23:21:02 -0700

    Login working

--- sms/__init__.py
+++ sms/__init__.py
@@ -3,8 +3,49 @@ The flask application package.
 """
 
 from flask import Flask
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "FLASK_SECRET_KEY"
+from flask_login import LoginManager
+from model.user import User
 
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg')
+#app.config['SECRET_KEY'] = "FLASK_SECRET_KEY"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login_page"
+
+
+# Our mock database.
+users = {'hkapoordroid@gmail.com': {'pw': 'test123'}}
+
+
+@login_manager.user_loader
+def load_user(email):
+    #TODO: here based on id, AWS RDS lookup needs to be done to get the data back to LoginManager
+    #user = User("hkapoordroid@gmail.com", "123456")
+    #return user
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('loemail')
+    if email not in users:
+        return
+    
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['pw'] == users[email]['pw']
+
+    return user
+    
 
 import sms.views
--- sms/views.py
+++ sms/views.py
@@ -1,16 +1,25 @@
 """
 Routes and views for the flask application.
 """
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request, redirect, url_for, g
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from datetime import datetime
-from sms import app
-from wtforms import StringField, BooleanField, SelectField, TextAreaField
+from sms import app, users
+from wtforms import StringField, BooleanField, SelectField, TextAreaField, PasswordField
+from wtforms.validators import InputRequired, Email
+from flask import flash
+from model.user import User
+from flask_login import login_user, login_required, current_user, logout_user
 import awshelper
 
 
 Types = ['Shoes', 'Watch', 'Hangbag', 'Clothes']
+
+
+class LoginForm(FlaskForm):
+    loemail = StringField("Email",  [InputRequired("Please enter your email address."), Email("This field requires a valid email address")])
+    lopass = PasswordField("Password", [InputRequired("Please enter your password.")])
 
 
 class UploadForm(FlaskForm):
@@ -20,18 +29,70 @@ class UploadForm(FlaskForm):
     gadescription = TextAreaField("description")
 
 
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@app.route('/', methods=['GET'])
+@login_required
+def index_page():
+    return redirect(url_for('upload_page'))
+    
+
+@app.route('/login', methods=['POST', 'GET'])
+def login_page():
+    form = LoginForm()
+    if request.method == 'POST' and not form.validate_on_submit():
+        flash('Invalid Login')
+    
+    if form.validate_on_submit():
+        useremail = form.loemail.data
+        userpass = form.lopass.data
+
+        print('Username = ' + useremail)
+        print('Password = ' + userpass)
+
+        #TODO: Authentication goes here
+
+        #if the authentication is success
+        #create an instance of User class 
+        #user = User(useremail, userpass)
+        #save the logged in user
+
+        if userpass == users[useremail]['pw']:
+            user = User()
+            user.id = useremail
+            login_user(user)
+            
+            #flask.flash('Logged in successfully.')
+            next = request.args.get('next')
+            return redirect(url_for('index_page'))
+        #else:
+        #TODO: If authentication fails, return error message to user
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET'])
+def logout_page():
+    logout_user()
+    return redirect(url_for('login_page'))    
+
+
 @app.route('/sga', methods=['POST', 'GET'])
+@login_required
 def upload_page():
     form = UploadForm()
     if form.validate_on_submit():
-        print(form.gaphoto.data.filename)
+        #print(form.gaphoto.data.filename)
         s3PhotoUrl = awshelper.upload_image_to_s3(form.gaphoto)
 
         #get the fields to display
-        rwtitle = str(form.gatitle.data)
-        rwdescription = str(form.gadescription.data)
-        rwtype = str(form.gatype.data)
-        rwphoto = "<img src=\"" + s3PhotoUrl + "\" height=\"150\" width=\"75\">"
+        #rwtitle = str(form.gatitle.data)
+        #rwdescription = str(form.gadescription.data)
+        #rwtype = str(form.gatype.data)
+        #rwphoto = "<img src=\"" + s3PhotoUrl + "\" height=\"150\" width=\"75\">"
         
 
         #now lets review the giveaway before final submission
